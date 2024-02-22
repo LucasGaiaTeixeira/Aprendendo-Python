d4a0aba7412ffd11bb3908431ed21893c5906c43
LucasGaiaTeixeira <lucasgaiatxra@gmail.com>
2024-02-22 18:35:38 -0300

    1 commit do aprendizado

---
+++ laco_while.py
@@ -0,0 +1,18 @@
+#while = enquanto == true
+    #vai executar um bloco de codigo
+
+'''
+print('inicio')
+
+soma = 0
+contagem = 0
+while soma <= 100:
+    print(f'a soma = {soma}')
+    soma -= 1
+    contagem += 1
+    if contagem == 20:
+        print('maximo de repetições')
+        break
+print('fim')
+'''
+
